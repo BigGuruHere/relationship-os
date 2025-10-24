209109f44601788c620448bd30ab7615ecb33d19
Terence Sweeney <terence@terencesweeney.com>
2025-10-24 16:26:59 +1100

    fixed the /index.html problem.

--- static/service-worker.js
+++ static/service-worker.js
@@ -1,14 +1,18 @@
 // static/service-worker.js
 // PURPOSE: basic PWA service worker to make the site installable and control caching
 
+// service-worker.js
+// PURPOSE: basic SW that pre-caches only files that actually exist in /static
+// SECURITY: no user data cached, only public assets
+
 self.addEventListener('install', (event) => {
-    // IT: pre-cache important assets if you like
+    // IT: cache only assets that live in /static
+    const urlsToCache = ['/', '/site.webmanifest', '/favicon.ico']; // remove '/index.html'
     event.waitUntil(
-      caches.open('relish-cache-v1').then((cache) => {
-        return cache.addAll(['/','/index.html','/site.webmanifest']);
-      })
+      caches.open('relish-cache-v1').then((cache) => cache.addAll(urlsToCache))
     );
   });
+  
   
   self.addEventListener('fetch', (event) => {
     // IT: basic network-first strategy
